16ca6df6ee01b72474d843c5a048af3c5535218c
Vterebenin <terebenin.work@gmail.com>
2019-12-08 23:32:38 +0300

    added simple front-end

--- laba_3/client/pages/index.vue
+++ laba_3/client/pages/index.vue
@@ -1,92 +1,68 @@
-<template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+<template lang="pug">
+  v-container
+    v-row
+      v-col.text-center(cols="12")
+        | Expert System Clone
+    v-row
+      v-col(cols="6")
+        v-tabs(grow v-model="tab" color="white")
+          v-tab Загрузить
+          v-tab Выбрать из сохраненных
+        v-tabs-items(v-model="tab")
+          v-tab-item
+            v-card
+              v-card-title Загрузите базу знаний
+              v-divider
+              v-form(@submit.prevent="getFormValues")
+                v-card-text
+                  v-text-field(name="name" label="Введите название базы знаний")
+                  v-file-input(name="file" label="Загрузить файл с базой знаний")
+                v-card-actions
+                  v-spacer
+                  v-btn(type="submit") Загрузить
+          v-tab-item
+            v-card
+              v-card-title Выберите базу знаний
+              v-divider
+              v-form
+                v-card-text
+                  v-list(v-if="files.length")
+                    v-list-item(v-for="(file, i) in files" :key="i")
+                  v-list(v-else)
+                    v-list-item К сожалению, базы знаний отсутствуют
+
+                v-card-actions
+                  v-spacer
+                  v-btn(typ="submit") Выбрать
+      v-col(cols="6")
+        v-card(max-height="200" )
+          v-card-title Результат
+          v-card-text {{ output.join("\r\n") }}
+
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  name: 'ExpertSystemIndex',
+  data: function () {
+    return {
+      tab: null,
+      files: [],
+      output: ['123', 'llorem ipsum lorem ipsum lorem ipsum lorem ipsum orem ipsum '],
+      name: '',
+      file: '',
+    }
+  },
+  methods: {
+    getFormValues(submitEvent) {
+      // console.log(submitEvent)
+      this.name = submitEvent.target.elements.name.value
+      this.file = submitEvent.target.elements.file.value
+
+
+    }
   }
+
 }
 </script>
